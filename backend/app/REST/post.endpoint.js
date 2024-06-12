9f4f57126fefb5112393ff8e9febce4996cac031
enqer <pawel.kam4@gmail.com>
2024-06-12 19:44:09 +0200

    feat: get post by id backend

--- backend/app/REST/post.endpoint.js
+++ backend/app/REST/post.endpoint.js
@@ -19,5 +19,20 @@ const postEndpoint = (router) => {
         }
     });
 
+    router.get('/api/posts/:id', async (request, response, next) => {
+        try {
+            const postId = request.params.id;
+            let result = await business.getPostManager().get(postId);
+            if (result) {
+                response.status(200).send(result);
+            } else {
+                response.status(404).send('Post Not Found');
+            }
+        } catch (error) {
+            console.log(error);
+            response.status(500).send('Internal Server Error');
+        }
+    });
+
 };
 export default postEndpoint;
